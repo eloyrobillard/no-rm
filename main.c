dc38da65008da05fc5e51b488f3dddd1c9a53e45
Eloy Robillard <eloy.robillard@gmail.com>
2025-11-03 11:12:54 +0100

    Enable --version option

--- main.c
+++ main.c
@@ -17,7 +17,10 @@ int main(int argc, char *argv[]) {
   strcat(cmd, "gio trash ");
 
   for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--force") == 0) {
+    if (strcmp(argv[i], "--version") == 0) {
+      cmd = "gio --version";
+      break;
+    } else if (strcmp(argv[i], "--force") == 0) {
       strcat(&cmd[end], "-f ");
       end += 3;
     } else if (strcmp(argv[i], "-f") == 0) {
