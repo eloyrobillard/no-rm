7030429688137ae0502433d66042e3b24195ac45
Eloy Robillard <eloy.robillard@gmail.com>
2025-11-04 10:25:14 +0100

    Refactor tests and check files get deleted

--- test.sh
+++ test.sh
@@ -18,35 +18,46 @@ should_not_be_empty() {
 }
 
 test() {
-  touch $1
-  "$2" $(./a.out $3)
+  declare -n arr=$1
+  declare -n arr2=$3
+
+  for FILE in "${arr[@]}"; do
+    touch "$FILE"
+  done
+
+  "$2" "$(./a.out "${arr2[@]}")"
+
+  for FILE in "${arr[@]}"; do
+    if [ -f "$FILE" ]; then
+        echo "エラー：$FILE は削除されませんでした。"
+    fi
+  done
 }
 
 make main
 
-touch ~/path
-out=$(./a.out ~/path)
-should_be_empty "$out"
+a=("$HOME/path" "path2")
+test a should_be_empty a
 
-touch path ~/path2
-out=$(./a.out path ~/path2)
-should_be_empty "$out"
+a=("path" "$HOME/path2")
+test a should_be_empty a
 
-touch path path2
-out=$(./a.out path -f --force path2)
-should_be_empty "$out"
+a=("pa  th")
+test a should_be_empty a
 
-touch path ~/path2 path3 -- -foo
-out=$(./a.out path -f --force ~/path2 -- -foo path3)
-should_be_empty "$out"
+a=("path" "path2")
+a2=("path" "-f" "--force" "path2")
+test a should_be_empty a2
 
-touch path
-out=$(./a.out path -i -I --interactive=never -r -R --recursive --one-file-system --no-preserve-root --preserve-root -d --dir -v --verbose)
-should_be_empty "$out"
+a=("path" "$HOME/path2" "./-foo" "path3")
+a2=("path" "-f" "--force" "$HOME/path2" "--" "-foo" "path3")
+test a should_be_empty a2
 
-touch path
-out=$(./a.out path --fake-option -foo 2>&1)
-should_not_be_empty "$out"
+a=("path")
+a2=("path" "-i" "-I" "--interactive=never" "-r" "-R" "--recursive" "--one-file-system" "--no-preserve-root" "--preserve-root" "-d" "--dir" "-v" "--verbose")
+test a should_be_empty a2
+a2=("path" "--fake-option" "2>&1")
+test a should_not_be_empty a2
 
 out=$(./a.out --version)
 should_not_be_empty "$out"
