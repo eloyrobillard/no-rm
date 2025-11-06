3f38d3531987009cd3e8e10a95612ee14b2895f7
Eloy Robillard <eloy.robillard@gmail.com>
2025-11-06 11:07:47 +0100

    Change test to test_file_trashed

--- test.sh
+++ test.sh
@@ -17,7 +17,7 @@ should_not_be_empty() {
   fi
 }
 
-test() {
+test_file_trashed() {
   declare -n arr=$1
   declare -n arr2=$3
 
@@ -37,25 +37,26 @@ test() {
 make main
 
 a=("$HOME/path" "path2")
-test a should_be_empty a
+test_file_trashed a should_be_empty a
 
 a=("path" "$HOME/path2")
-test a should_be_empty a
+test_file_trashed a should_be_empty a
 
 a=("pa  th")
-test a should_be_empty a
+test_file_trashed a should_be_empty a
 
 a=("path" "path2")
 a2=("path" "-f" "--force" "path2")
-test a should_be_empty a2
+test_file_trashed a should_be_empty a2
 
 a=("path" "$HOME/path2" "./-foo" "path3")
 a2=("path" "-f" "--force" "$HOME/path2" "--" "-foo" "path3")
-test a should_be_empty a2
+test_file_trashed a should_be_empty a2
 
 a=("path")
 a2=("path" "-i" "-I" "--interactive=never" "-r" "-R" "--recursive" "--one-file-system" "--no-preserve-root" "--preserve-root" "-d" "--dir" "-v" "--verbose")
-test a should_be_empty a2
+test_file_trashed a should_be_empty a2
+
 a2=("path" "--fake-option" "2>&1")
 test a should_not_be_empty a2
 
